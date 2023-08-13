fbfff9c3fde723fcef5a436e5c3a5f7de2d8d35a
kanchan raju <kanchanraju2015@gmail.com>
2023-08-13 16:23:48 +0530

    this is my first commit of book test

---
+++ src/test/java/com/briz/BookTest/BookTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.briz.BookTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
