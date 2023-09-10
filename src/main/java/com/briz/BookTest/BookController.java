487e287a00affc8325145f9f8b447f5460067f9e
kanchan raju <kanchanraju2015@gmail.com>
2023-09-10 10:25:32 +0530

    service commit

--- src/main/java/com/briz/BookTest/BookController.java
+++ src/main/java/com/briz/BookTest/BookController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class BookController 
 {
 @Autowired
-BookRepository brepo;
+BookService bserv;
 @RequestMapping("/booktest")
 public String test()
 {
--- src/main/java/com/briz/BookTest/BookServiceImpl.java
+++ src/main/java/com/briz/BookTest/BookServiceImpl.java
@@ -1,5 +1,47 @@
 package com.briz.BookTest;
 
-public class BookServiceImpl {
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service 
+public class BookServiceImpl implements BookService 
+{
+	@Autowired
+	BookRepository brepo;
+
+	@Override
+	public void addBook(Book book)
+	{
+	brepo.save(book);
+	System.out.println("book saved into database");
+		
+	}
+
+	@Override
+	public List<Book> allBooks() {
+		return brepo.findAll();
+	}
+
+	@Override
+	public Book updateBook(Book book, int id)
+	{
+	Book b=brepo.findById(id).get();
+	b.setBookauthor(book.getBookauthor());
+	b.setBookname(book.getBookauthor());
+	b.setBooksubject(book.getBooksubject());
+	brepo.save(b);
+	
+	return b;
+	}
+
+	@Override
+	public Book deleteBook(int id) {
+		
+		Book book=brepo.findById(id).get();
+		brepo.deleteById(id);
+		return book;
+			}
 
 }
